228c1a65a3605d5936de7a2987a4139addb75fa4
quedevel <kdh90817@naver.com>
2024-01-18 17:09:42 +0900

    feat: UI 요소 가져오기
    * viewBinding

--- chapter3/app/src/main/java/code/wave/chapter3/MainActivity.kt
+++ chapter3/app/src/main/java/code/wave/chapter3/MainActivity.kt
@@ -2,10 +2,19 @@ package code.wave.chapter3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import code.wave.chapter3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    val view = binding.root
+    setContentView(view)
+
+    val inputEditText = binding.inputEditText
+    val outputTextView = binding.outputTextView
+    val inputUnitTextView = binding.inputUnitTextView
+    val outputUnitTextView = binding.outputUnitTextView
   }
 }
