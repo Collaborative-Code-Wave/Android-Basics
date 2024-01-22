63bb34c8fff5f18a386e64dd321a3ca7e202cc09
quedevel <kdh90817@naver.com>
2024-01-22 11:18:09 +0900

    feat: 응급의료 정보 UI 그리기 3
    * multiple layer
    * DatePickerDialog
    * setOnCheckedChangeListener
    * isChecked
    * isVisible

--- chapter4/app/src/main/java/code/wave/chapter4/EditActivity.kt
+++ chapter4/app/src/main/java/code/wave/chapter4/EditActivity.kt
@@ -1,15 +1,37 @@
 package code.wave.chapter4
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import androidx.core.view.isVisible
+import code.wave.chapter4.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityEditBinding
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_edit)
+    binding = ActivityEditBinding.inflate(layoutInflater)
+    setContentView(binding.root)
 
-    val message = intent.getStringExtra("intentMessage") ?: "None"
-    Log.d("EditActivity", message)
+    binding.bloodTypeSpinner.adapter = ArrayAdapter.createFromResource(
+      this, R.array.blood_types, android.R.layout.simple_list_item_1
+    )
+
+    binding.birthdateLayer.setOnClickListener {
+      val listener = OnDateSetListener{ _, year, month, dayOfMonth ->
+        binding.birthdateValueTextView.text = "$year-${month.inc()}-$dayOfMonth"
+      }
+      DatePickerDialog(this, listener, 2000, 1, 1).show()
+    }
+
+    binding.warningCheckBox.setOnCheckedChangeListener { _, isChecked ->
+      binding.warningEditTextView.isVisible = isChecked
+    }
+
+    binding.warningEditTextView.isVisible = binding.warningCheckBox.isChecked
+
   }
 }
