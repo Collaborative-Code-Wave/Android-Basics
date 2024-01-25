533d78a9d789a80818f76e84d9a363f36a3b91b8
quedevel <kdh90817@naver.com>
2024-01-25 15:59:17 +0900

    feat: 단어장 UI 그리기 3
    * RecyclerView
    * Adaptor

--- chapter7/app/src/main/java/code/wave/chapter7/AddActivity.kt
+++ chapter7/app/src/main/java/code/wave/chapter7/AddActivity.kt
@@ -3,6 +3,7 @@ package code.wave.chapter7
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import code.wave.chapter7.databinding.ActivityAddBinding
+import com.google.android.material.chip.Chip
 
 class AddActivity : AppCompatActivity() {
   private lateinit var binding: ActivityAddBinding
@@ -10,5 +11,24 @@ class AddActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     binding = ActivityAddBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+    initViews()
+  }
+
+  private fun initViews() {
+    val types = listOf(
+      "명사", "동사", "대명사", "형용사", "부사", "감탄사", "전치사", "접속사"
+    )
+    binding.typeChipGroup.apply {
+      types.forEach { text -> addView(createChip(text)) }
+    }
+  }
+
+  private fun createChip(text: String): Chip {
+    return Chip(this).apply {
+      setText(text)
+      isCheckable = true
+      isClickable = true
+    }
   }
 }
---
+++ chapter7/app/src/main/java/code/wave/chapter7/Word.kt
@@ -0,0 +1,7 @@
+package code.wave.chapter7
+
+data class Word(
+  val text: String,
+  val mean: String,
+  val type: String,
+)
