6c6a86309f327b80c1d29e489b2510015d2af2f6
quedevel <kdh90817@naver.com>
2024-01-26 17:05:01 +0900

    feat: 갤러리에서 이미지 불러오기

--- chapter8/app/src/main/java/code/wave/chapter8/MainActivity.kt
+++ chapter8/app/src/main/java/code/wave/chapter8/MainActivity.kt
@@ -2,10 +2,13 @@ package code.wave.chapter8
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
@@ -16,6 +19,10 @@ class MainActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityMainBinding
 
+  private val imageLoadLauncher = registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uriList ->
+    updateImages(uriList)
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,6 +32,10 @@ class MainActivity : AppCompatActivity() {
       checkPermission()
     }
 
+  }
+
+  private fun updateImages(uriList: List<Uri>){
+    Log.i("updateImages", "$uriList")
   }
 
   private fun checkPermission() {
@@ -46,17 +57,17 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
-  private fun showPermissionInfoDialog(){
+  private fun showPermissionInfoDialog() {
     AlertDialog.Builder(this).apply {
       setMessage("이미지를 가져오기 위해서, 외부 저장소 읽기 권한이 필요합니다.")
       setNegativeButton("취소", null)
-      setPositiveButton("동의"){ _, _ ->
+      setPositiveButton("동의") { _, _ ->
         requestReadMediaImages()
       }
     }.show()
   }
 
-  private fun requestReadMediaImages(){
+  private fun requestReadMediaImages() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
       ActivityCompat.requestPermissions(
         this,
@@ -66,10 +77,20 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
+  override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    when(requestCode) {
+      REQUEST_READ_EXTERNAL_STORAGE -> {
+        if (grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED){
+          loadImage()
+        }
+      }
+    }
+  }
 
 
   private fun loadImage() {
-    Toast.makeText(this, "이미지를 가져올 예정", Toast.LENGTH_SHORT).show()
+    imageLoadLauncher.launch("image/*")
   }
 
   companion object {
