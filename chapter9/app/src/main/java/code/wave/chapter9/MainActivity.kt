07e9839bf6a2c85dae1618182c2f18ca566eb4a1
quedevel <kdh90817@naver.com>
2024-01-29 13:16:04 +0900

    feat: media player project init

---
+++ chapter9/app/src/main/java/code/wave/chapter9/MainActivity.kt
@@ -0,0 +1,15 @@
+package code.wave.chapter9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import code.wave.chapter9.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+  }
+}
