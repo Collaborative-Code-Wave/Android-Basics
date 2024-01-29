fac60a3f1f6fb2693e11a88bd83b4d8c14358430
quedevel <kdh90817@naver.com>
2024-01-29 13:49:43 +0900

    feat: MediaPlayer 로 음원재생 1

--- chapter9/app/src/main/java/code/wave/chapter9/MainActivity.kt
+++ chapter9/app/src/main/java/code/wave/chapter9/MainActivity.kt
@@ -1,15 +1,40 @@
 package code.wave.chapter9
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import code.wave.chapter9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
+  private var mediaPlayer: MediaPlayer? = null
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
+    binding.playButton.setOnClickListener { mediaPlayerPlay() }
+    binding.stopButton.setOnClickListener { mediaPlayerStop() }
+    binding.pauseButton.setOnClickListener { mediaPlayerPause() }
+
+  }
+
+  private fun mediaPlayerPause(){
+    mediaPlayer?.pause()
+  }
+
+  private fun mediaPlayerStop(){
+    mediaPlayer?.stop()
+    mediaPlayer?.release()
+    mediaPlayer = null
+  }
+
+  private fun mediaPlayerPlay() {
+    if (mediaPlayer == null){
+      mediaPlayer = MediaPlayer.create(this, R.raw.file_example).apply {
+        isLooping = true
+      }
+    }
+    mediaPlayer?.start()
   }
 }
