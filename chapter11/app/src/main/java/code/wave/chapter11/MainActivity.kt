f5f9ee32817b2ddc3e3ae1905076e2d06ecb7275
quedevel <kdh90817@naver.com>
2024-02-01 10:24:45 +0900

    feat: OKHttp 를 이용하여 Client 앱 만들기
    execute 동기
    APP 에서는 Main UI Thread 에서 Http 호출을 할 수 없다
    because : 동기화를 통해 HTTP 통신 진행중 에러 발생으로 UI 노출시 오류가 발생할 수 있기 때문이다.
    enqueue 비동기
    비동기 작업시 callback 을 구현해야 한다.

--- chapter11/app/src/main/java/code/wave/chapter11/MainActivity.kt
+++ chapter11/app/src/main/java/code/wave/chapter11/MainActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import code.wave.chapter11.databinding.ActivityMainBinding
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.PrintWriter
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 import java.lang.Exception
-import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,32 +21,56 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
-    Thread {
-      try{
-        val socket = Socket("10.0.2.2", 8080)
-        val printer = PrintWriter(socket.getOutputStream())
-        val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+    val client = OkHttpClient()
 
-        printer.println("GET / HTTP/1.1")
-        printer.println("Host: 127.0.0.1:8080")
-        printer.println("User-Agent: android")
-        printer.println("\r\n")
-        printer.flush()
+    val request= Request.Builder()
+      .url("http://10.0.2.2:8080")
+      .build()
 
-        var input: String? = "-1"
-        while (input != null) {
-          input = reader.readLine()
-          if (input != null) {
-            Log.e("Client", input)
-          }
-        }
-
-        reader.close()
-        printer.close()
-        socket.close()
-      } catch (e : Exception){
+    val callback = object : Callback {
+      override fun onFailure(call: Call, e: IOException) {
         Log.e("Client", e.toString())
       }
-    }.start()
+      override fun onResponse(call: Call, response: Response) {
+        if (response.isSuccessful){
+          Log.e("Client", "${response.body?.string()}")
+        }
+      }
+    }
+
+    // execute 동기
+    // APP 에서는 Main UI Thread 에서 Http 호출을 할 수 없다
+    // because : 동기화를 통해 HTTP 통신 진행중 에러 발생으로 UI 노출시 오류가 발생할 수 있기 때문이다.
+    // enqueue 비동기
+    // 비동기 작업시 callback 을 구현해야 한다.
+    client.newCall(request).enqueue(callback)
+
+//    Thread {
+//      try{기
+//        val socket = Socket("10.0.2.2", 8080)
+//        val printer = PrintWriter(socket.getOutputStream())
+//        val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+//
+//        printer.println("GET / HTTP/1.1")
+//        printer.println("Host: 127.0.0.1:8080")
+//        printer.println("User-Agent: android")
+//        printer.println("\r\n")
+//        printer.flush()
+//
+//        var input: String? = "-1"
+//        while (input != null) {
+//          input = reader.readLine()
+//          if (input != null) {
+//            Log.e("Client", input)
+//          }
+//        }
+//
+//        reader.close()
+//        printer.close()
+//        socket.close()
+//      } catch (e : Exception){
+//        Log.e("Client", e.toString())
+//      }
+//    }.start()
   }
 }
