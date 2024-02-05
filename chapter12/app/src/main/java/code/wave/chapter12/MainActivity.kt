ccf6e2e13f2b1e2cd6fe53ce4c1dfbae14aabb27
quedevel <kdh90817@naver.com>
2024-02-05 10:23:07 +0900

    feat: Retrofit 을 이용해 서버 데이터 불러오기

--- chapter12/app/src/main/java/code/wave/chapter12/MainActivity.kt
+++ chapter12/app/src/main/java/code/wave/chapter12/MainActivity.kt
@@ -2,7 +2,16 @@ package code.wave.chapter12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import code.wave.chapter12.databinding.ActivityMainBinding
+import code.wave.chapter12.model.Repo
+import code.wave.chapter12.network.GithubService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +21,22 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+    val retrofit = Retrofit.Builder()
+      .baseUrl("https://api.github.com/")
+      .addConverterFactory(GsonConverterFactory.create())
+      .build()
+
+    val githubService = retrofit.create(GithubService::class.java)
+    githubService.listRepos("quedevel").enqueue(object : Callback<List<Repo>>{
+      override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+        Log.e("MainActivity", response.body().toString())
+      }
+
+      override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+        TODO("Not yet implemented")
+      }
+
+    })
   }
 }
---
+++ chapter12/app/src/main/java/code/wave/chapter12/network/GithubService.kt
@@ -0,0 +1,13 @@
+package code.wave.chapter12.network
+
+import code.wave.chapter12.model.Repo
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GithubService {
+
+  @GET("users/{username}/repos")
+  fun listRepos(@Path("username") username: String): Call<List<Repo>>
+
+}
