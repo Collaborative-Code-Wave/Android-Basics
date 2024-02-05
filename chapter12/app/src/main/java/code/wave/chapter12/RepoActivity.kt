6765d8650c3f583441278454226e3dbe69a12552
quedevel <kdh90817@naver.com>
2024-02-05 14:55:37 +0900

    feat: 레포지토리 조회 화면 구현하기 1

--- chapter12/app/src/main/java/code/wave/chapter12/RepoActivity.kt
+++ chapter12/app/src/main/java/code/wave/chapter12/RepoActivity.kt
@@ -2,13 +2,47 @@ package code.wave.chapter12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import code.wave.chapter12.adapter.RepoAdapter
 import code.wave.chapter12.databinding.ActivityRepoBinding
+import code.wave.chapter12.model.Repo
+import code.wave.chapter12.network.ApiClient
+import code.wave.chapter12.network.GithubService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RepoActivity : AppCompatActivity() {
   private lateinit var binding: ActivityRepoBinding
+  private lateinit var repoAdapter: RepoAdapter
+
+  private val githubService = ApiClient.getApiClient().create(GithubService::class.java)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityRepoBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+    val username = intent.getStringExtra("username") ?: return
+    binding.usernameTextView.text = username
+
+    repoAdapter = RepoAdapter()
+
+    binding.repoRecyclerView.apply {
+      layoutManager = LinearLayoutManager(this@RepoActivity)
+      adapter = repoAdapter
+    }
+
+    listRepo(username)
+  }
+
+  private fun listRepo(username: String) {
+    githubService.listRepos(username).enqueue(object : Callback<List<Repo>> {
+      override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+        repoAdapter.submitList(response.body())
+      }
+      override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+      }
+    })
   }
 }
---
+++ chapter12/app/src/main/java/code/wave/chapter12/adapter/RepoAdapter.kt
@@ -0,0 +1,47 @@
+package code.wave.chapter12.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import code.wave.chapter12.databinding.ItemRepoBinding
+import code.wave.chapter12.model.Repo
+
+class RepoAdapter: ListAdapter<Repo, RepoAdapter.RepoViewHolder>(diffUtil) {
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoViewHolder {
+    return RepoViewHolder(
+      ItemRepoBinding.inflate(
+        LayoutInflater.from(parent.context),
+        parent,
+        false
+      )
+    )
+  }
+
+  override fun onBindViewHolder(holder: RepoViewHolder, position: Int) {
+    holder.bind(currentList[position])
+  }
+
+  inner class RepoViewHolder(private val binding: ItemRepoBinding): RecyclerView.ViewHolder(binding.root){
+    fun bind(item: Repo){
+      binding.repoNameTextView.text = item.name
+      binding.descriptionTextView.text = item.description
+      binding.forkCountTextView.text = item.forkCount.toString()
+      binding.startCountTextView.text = item.starCount.toString()
+    }
+  }
+
+  companion object {
+    val diffUtil = object: DiffUtil.ItemCallback<Repo>() {
+      override fun areItemsTheSame(oldItem: Repo, newItem: Repo): Boolean {
+        return oldItem.id == newItem.id
+      }
+
+      override fun areContentsTheSame(oldItem: Repo, newItem: Repo): Boolean {
+        return oldItem == newItem
+      }
+    }
+  }
+}
